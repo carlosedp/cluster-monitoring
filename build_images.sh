26fe27848558acbbb2e31983be7411028892612d
CarlosEDP <me@carlosedp.com>
2018-06-18 16:53:48 -0500

    Updated build script

--- build_images.sh
+++ build_images.sh
@@ -2,9 +2,13 @@
 
 REPO=carlosedp
 
-# Retag Addon-resizer google images to have unified manifest on DockerHub
 AOM_VERSION=2.1
+KSM_VERSION=v1.3.0
+VERSION=v0.20.0
+PROMCONFIGRELOADER_VERSION=v0.20.0
 
+# Kubernetes addon-resizer
+# Retag Addon-resizer google images to have unified manifest on DockerHub
 docker pull gcr.io/google-containers/addon-resizer-arm64:$AOM_VERSION
 docker pull gcr.io/google-containers/addon-resizer-arm:$AOM_VERSION
 
@@ -17,9 +21,7 @@ docker push $REPO/addon-resizer:$AOM_VERSION-arm64
 manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/addon-resizer:$AOM_VERSION-ARCH --target $REPO/addon-resizer:$AOM_VERSION
 manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/addon-resizer:$AOM_VERSION-ARCH --target $REPO/addon-resizer:latest
 
-
 # Kube-state-metrics
-KSM_VERSION=v1.3.0
 go get github.com/kubernetes/kube-state-metrics
 mv $HOME/go/src/github.com/kubernetes/kube-state-metrics $HOME/go/src/k8s.io/kube-state-metrics
 cd $HOME/go/src/k8s.io/kube-state-metrics
@@ -39,16 +41,13 @@ manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --tem
 
 # Prometheus-operator
 
-VERSION=v0.20.0
 go get github.com/coreos/prometheus-operator
 cd $HOME/go/src/github.com/coreos/prometheus-operator
 git checkout ${VERSION}
 
-
 go get -u github.com/prometheus/promu
 
 cat Dockerfile |sed -e 's/\.build\/linux-amd64\/operator/operator/' |sed -e 's/^FROM.*/FROM busybox/' > Dockerfile.arm
-
 
 GOOS=linux GOARCH=arm $GOPATH/bin/promu build --prefix `pwd`
 docker build -t $REPO/prometheus-operator:${VERSION}-arm -f Dockerfile.arm .
@@ -65,25 +64,23 @@ manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --tem
 rm Dockerfile.arm
 
 # prometheus-config-reloader
-
-VERSION=v0.20.0
 go get github.com/coreos/prometheus-operator
 cd $HOME/go/src/github.com/coreos/prometheus-operator/
-git checkout ${VERSION}
+git checkout ${PROMCONFIGRELOADER_VERSION}
 cd $HOME/go/src/github.com/coreos/prometheus-operator/contrib/prometheus-config-reloader
 
 cat Dockerfile |sed -e 's/^FROM.*/FROM busybox/' > Dockerfile.arm
 
 GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o prometheus-config-reloader main.go
-docker build -t $REPO/prometheus-config-reloader:${VERSION}-arm -f Dockerfile.arm .
+docker build -t $REPO/prometheus-config-reloader:${PROMCONFIGRELOADER_VERSION}-arm -f Dockerfile.arm .
 
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o prometheus-config-reloader main.go
-docker build -t $REPO/prometheus-config-reloader:${VERSION}-arm64 -f Dockerfile.arm .
+docker build -t $REPO/prometheus-config-reloader:${PROMCONFIGRELOADER_VERSION}-arm64 -f Dockerfile.arm .
 
-docker push $REPO/prometheus-config-reloader:$VERSION-arm
-docker push $REPO/prometheus-config-reloader:$VERSION-arm64
+docker push $REPO/prometheus-config-reloader:$PROMCONFIGRELOADER_VERSION-arm
+docker push $REPO/prometheus-config-reloader:$PROMCONFIGRELOADER_VERSION-arm64
 
-manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/prometheus-config-reloader:$VERSION-ARCH --target $REPO/prometheus-config-reloader:$VERSION
-manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/prometheus-config-reloader:$VERSION-ARCH --target $REPO/prometheus-config-reloader:latest
+manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/prometheus-config-reloader:$PROMCONFIGRELOADER_VERSION-ARCH --target $REPO/prometheus-config-reloader:$VERSION
+manifest-tool-linux-arm64 push from-args --platforms linux/arm,linux/arm64 --template $REPO/prometheus-config-reloader:$PROMCONFIGRELOADER_VERSION-ARCH --target $REPO/prometheus-config-reloader:latest
 
 rm Dockerfile.arm
